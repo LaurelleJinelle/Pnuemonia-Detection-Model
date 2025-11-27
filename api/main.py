1232996284b9a66ffc935cdc4e98ae7476ca9892
LaurelleJinelle <156344054+LaurelleJinelle@users.noreply.github.com>
2025-11-27 22:04:58 +0200

    update model path

--- api/main.py
+++ api/main.py
@@ -38,7 +38,7 @@ num_predictions = 0
 last_retrain_time = None
 
 # ✔ Load fine-tuned SavedModel
-current_model_path = os.path.join(MODELS_DIR, "mobilenet_pneumonia_finetuned.h5")
+current_model_path = os.path.join(MODELS_DIR, "mobilenet_pneumonia_finetuned_model.h5")
 prediction.load_model(current_model_path)
 
 # --------------------
@@ -115,4 +115,5 @@ async def retrain():
         "message": "Retraining complete.",
         "new_model_path": new_model_path,
         "history": history,
+
     }
