65e1cc13163a19de33ff8716ef5ebdfed1ef1294
LaurelleJinelle <156344054+LaurelleJinelle@users.noreply.github.com>
2025-11-28 22:40:10 +0200

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -2,16 +2,17 @@ from locust import HttpUser, task, between
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-TEST_IMAGE = os.path.join(BASE_DIR, "sample_images", "normal1.jpg")
+TEST_IMAGE = os.path.join(BASE_DIR, "sample_images", "normal1.jpeg")
 
-class PredictUser(HttpUser):
+class PneumoniaLoadTest(HttpUser):
     wait_time = between(1, 5)
 
     @task
-    def predict(self):
+    def test_predict(self):
         if not os.path.exists(TEST_IMAGE):
+            print("[ERROR] File NOT FOUND →", TEST_IMAGE)
             return
-        with open(TEST_IMAGE, "rb") as f:
-            files = {"file": ("sample.png", f, "image/png")}
-            self.client.post("/predict", files=files)
 
+        with open(TEST_IMAGE, "rb") as f:
+            files = {"file": ("normal1.jpeg", f, "image/jpeg")}
+            self.client.post("/predict", files=files)
