607dc62f0f1846aaf49141c87934d322f897ecc2
LaurelleJinelle <156344054+LaurelleJinelle@users.noreply.github.com>
2025-11-28 23:25:23 +0200

    Update prediction.py

--- src/prediction.py
+++ src/prediction.py
@@ -2,9 +2,11 @@ import tensorflow as tf
 import numpy as np
 from src.preprocessing import preprocess_for_prediction
 from keras import models as keras_models 
+import threading
+predict_lock = threading.Lock()
 
 _model = None
-_class_names = ["NORMAL", "PNEUMONIA"]   # adjust based on your dataset
+_class_names = ["NORMAL", "PNEUMONIA"]   
 
 def load_model(model_path: str):
     global _model
@@ -12,11 +14,18 @@ def load_model(model_path: str):
     _model = keras_models.load_model(model_path)
 
 def predict_image_bytes(img_bytes: bytes):
+    global _model
+    
     img = preprocess_for_prediction(img_bytes)
-    preds = _model.predict(img)[0]
+    with predict_lock:
+        preds = _model.predict(img)[0]
+
     class_idx = np.argmax(preds)
     confidence = float(np.max(preds))
     label = _class_names[class_idx]
+
     return label, confidence
+
+
 
 
