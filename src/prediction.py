2068064fcb03cf8cc59f0d333b1043f35c335016
LaurelleJinelle <156344054+LaurelleJinelle@users.noreply.github.com>
2025-11-28 21:31:06 +0200

    Update prediction.py

--- src/prediction.py
+++ src/prediction.py
@@ -8,7 +8,7 @@ _class_names = ["NORMAL", "PNEUMONIA"]   # adjust based on your dataset
 def load_model(model_path: str):
     global _model
     print(f"[INFO] Loading model from {model_path}")
-    _model = tf.keras.models.load_model(model_path)
+    _model = keras_models.load_model(model_path)
 
 def predict_image_bytes(img_bytes: bytes):
     img = preprocess_for_prediction(img_bytes)
@@ -17,3 +17,4 @@ def predict_image_bytes(img_bytes: bytes):
     confidence = float(np.max(preds))
     label = _class_names[class_idx]
     return label, confidence
+
